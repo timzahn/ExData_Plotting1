ce66b771b8d6649138e85d8c399b95711dc7748a
AWE <alexander.weinhard@gmail.com>
2015-08-04 09:58:46 +0200

    first commit

---
+++ Plot1.R
@@ -0,0 +1,18 @@
+setwd("C:\\Users\\alw64gf\\Documents\\R\\ExploratoryAnalysis\\Week1")
+power_data<- read.csv(".\\data\\household_power_consumption.csv", sep=";", na.strings = "?")
+
+power_data$dateTime <- strptime(paste(power_data$Date, power_data$Time), "%d/%m/%Y %H:%M:%S")
+
+#create boundaries for subsetting
+under_boundary <-strptime("2007-02-01 00:00:00",format="%Y-%m-%d %H:%M:%S")
+upper_boundary <-strptime("2007-02-02 23:59:59",format="%Y-%m-%d %H:%M:%S")
+
+#subset the dataset
+dataset <- subset(power_data, upper_boundary >= power_data$dateTime & power_data$dateTime >= under_boundary)
+
+
+#Create first graph
+hist(dataset$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot1.png")
+dev.off()
+
